6f0cadf05f2f4eaf0ee9eede59c3c2f5690a4d6f
FadellMuhammad <fadel54031@gmail.com>
2021-07-01 20:54:59 +0700

    add pages services, values and update icon, title.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import { Router } from "@reach/router";
 import Layout from "./components/layout/Layout";
 import Header from "./components/header/Header";
+import { Helmet } from "react-helmet";
 // import { Home }  from "./pages";
 
 const App = () => {
   return (
-    <Router>
-      <Layout path="/" />
-      <Header path="/header" />
-    </Router>
+    <>
+      <Helmet defaultTitle="Intens">
+        <meta charSet="utf-8" />
+      </Helmet>
+      <Router>
+        <Layout path="/" />
+        <Header path="/header" />
+      </Router>
+    </>
   )
 }
 
