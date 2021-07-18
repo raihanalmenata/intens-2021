939326a0d95cde2fa2b157ce47e01769b6644742
FadellMuhammad <fadel54031@gmail.com>
2021-07-18 23:07:58 +0700

    add pages {family, contact, clients}

---
+++ src/pages/family/Family.js
@@ -0,0 +1,25 @@
+import React from "react";
+import iconFamily from "../../assets/img/icon-family.png";
+
+import ListFamily from "./component/ListFamily";
+import "./Family.less";
+
+const Family = () => {
+    return (
+        <div className="family">
+            <div className="container">
+                <div className="text-content">
+                    <div className="text">
+                        <p>We aren’t just coworkers</p>
+                        <h1>We are family</h1>
+                        <p> <spam className="red">Professionals</spam> behind the scene.</p>
+                    </div>
+                    <img src={iconFamily} alt="icon-family" />
+                </div>
+                <ListFamily />
+            </div>
+        </div>
+    )
+}
+
+export default Family;
